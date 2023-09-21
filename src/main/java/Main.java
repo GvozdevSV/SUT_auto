e5c6f58be07b84fe79e13dbe88fdfe5f892bb26e
GvozdevSV <agasfen82@yandex.ru>
2023-09-21 05:57:38 +0400

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import  org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args){
+        System.setProperty("webdriver.crome.driver", "C:\\Users\\Asus\\IdeaProjects\\SUT_auto\\src\\main\\resources\\chromedriver-win32\\chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://github.com/");}
+}
